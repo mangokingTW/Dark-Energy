e4ff558ee0dcb370ab6a565748b2aca69314f491
Mango Yen <mango.yen@sifive.com>
2024-03-22 18:06:32 +0800

    calculate_Reaction_Probability

--- DarkEnergy.py
+++ DarkEnergy.py
@@ -89,6 +89,36 @@ def calculate_Vacuum_Energy(m):
     return res[energy_density]
 
 
+def calculate_Power_Density(wave_length):
+    frequency = 1 / wave_length
+    power_density, k = symbols('power_density, k', real=True)
+    eq = [
+        Eq(power_density, frequency ** 5 * h / pi ** 4 / c * eV2J)
+    ]
+    res = solve(eq, power_density, dict=True)[0]
+    return res[power_density]
+
+
+def calculate_Reaction_Probability(wave_length):
+    frequency = 1 / wave_length
+    probability, k = symbols('probability, k', real=True)
+    eq = [
+        Eq(probability, h*c*pi**3*Sum(1/(k*frequency)**3, (k, 1, oo)).doit())
+    ]
+    res = solve(eq, probability, dict=True)[0]
+    return res[probability]
+
+
+def calculate_Power(wave_length):
+    frequency = 1 / wave_length
+    power, k = symbols('power, k', real=True)
+    eq = [
+        Eq(power, frequency ** 2 * h / (pi * c))
+    ]
+    res = solve(eq, power, dict=True)[0]
+    return res[power]
+
+
 def distance_from_redshift(redshift):
     distance = symbols('distance', real=True)
     eq = [
@@ -171,18 +201,21 @@ def doit():
     """
     Critical density of the universe
     """
-    printE("Critical density", 3 * (H0) ** 2 / 8 / pi / G * kg2J)
+    printE("Critical density", 3 * (H0) ** 2 / 8 / pi / G * kg2J, "J/m3")
     """
     E_vac/V_vac ∝ photon/L^3, L ∝ V^0.5, h*c/L/V = h*c*L^-0.5 = h*c^0.5 if L = c
     """
-    printE("Redshift Energy", h * c ** 6 * pi / 2 * eV2J * SN_2008D['distance'])
-    printE("Redshift Energy Density", SN_2006gy['energy_redshift'] / SN_2006gy['distance'] / (4 / 3 * pi * RU ** 3))
+    printE("Redshift Energy", h * c ** 6 * pi / 2 * eV2J * SN_2008D['distance'], "J")
+    printE("Redshift Energy Density", SN_2006gy['energy_redshift'] / SN_2006gy['distance'] / (4 / 3 * pi * RU ** 3),
+           "J/m3")
+    printE("Redshift Power", SN_2006gy['energy_redshift'] * c / SN_2006gy['distance'], "J/s")
+    printE("Redshift Power Density", SN_2006gy['energy_redshift'] * c / SN_2006gy['distance'] / (4 / 3 * pi * RU ** 3), "J/m3s")
     """
     P_vac = E*Area/Volume = (1/2)*h*c/λ * (pi*r^2) / (4/3*pi*r^3) => (if λ = c^-1,r = c^-1) => (3/8)*h*c^3 * ev2J
     """
     R_ved = c ** -1
     P_vac = (1 / 2) * h * c / R_ved * (pi * R_ved ** 2) / (4 / 3 * pi * R_ved ** 3) * eV2J
-    printE("Vacuum Energy Density", P_vac)
+    printE("Guessed Vacuum Energy Density", P_vac, "J/m3")
     """
     = printE((3 / 8) * h * c ** 3 * eV2J)
     = printE(-calculate_Casimir_Effect(2.09922509e-5))
@@ -196,22 +229,21 @@ def doit():
     # New Hubble constant
     H0_new = sqrt(16 * pi * G / 3 * P_vac_A)
     printE("New Hubble constant", H0_new)
-    printE("Critical density", 3 * H0_new ** 2 / 8 / pi / G * kg2J)
+    printE("Critical density of new Hubble constant", 3 * H0_new ** 2 / 8 / pi / G * kg2J, "J/m3")
 
     # 量子漲落時間 Qt = hbar/2/Energy
     # c * Qt = radius
     # 測算真空能量密度： 5.18e44 J/m3
     # 中微子能量 < 0.8 eV 約等同波長 1.55e-6 的光
     # 能量總和 1.675462e+04
-    def calculate_quatum_flucatation():
-        t = hbar / 2 / (0.8 * eV2J)
-        r = c * t
-        wl = 1.55e-6
-        k = symbols('k', positive=True)
-        printE("Vacuum Energy", Sum(k, (k, 1, wl / lp)).doit() / wl * (1 / 2) * h * c / (4 / 3 * pi * r ** 3) * eV2J)
-
-    calculate_quatum_flucatation()
-    printE(calculate_Casimir_Effect(1e-8))
+    # Wave length =  h * c / Neutrino mass
+    # Neutrino < 0.8 ev => Wave length > 1.55e-6
+    printE("Neutrino's wave length", h * c / 0.8, "m")
+    printE("Power density of QF to create Neutrino", calculate_Power_Density(1.55e-6), "J/m3s")
+    printE("Power density of QF to create X", calculate_Power_Density(1.6e-9), "J/m3s")
+    printE(h * c / 1.6e-9)
+    printE("Power of QF to create X", calculate_Power(1.6e-9), "J/s")
+    printE("Probability of QF to create X", calculate_Reaction_Probability(c), "J")
 
 
 if __name__ == '__main__':
--- utils.py
+++ utils.py
@@ -1,5 +1,6 @@
-def printE(name, f=0):
+def printE(name, f=0, unit=""):
     if type(name) is not str:
         f = name
         name = None
-    print(str(f"{name}: " if name else "") + format(float(f), "e"))
+    print(str(f"{name}: " if name else "") + format(float(f), "e") + (f" {unit}" if unit else ""))
+    return f
