58a22d8ae39a0d7aa7cc57ea08baa7bd204065fb
Mango Yen <yench@cs.nctu.edu.tw>
2024-04-15 04:10:00 +0800

    update

--- VacuumEnergyAndDarkMatter.py
+++ VacuumEnergyAndDarkMatter.py
@@ -7,13 +7,30 @@ from utils import *
 # printE(2.443e18*J2eV/(4/3*pi*(138e9*ly2m**3)))
 
 L = 8.8e26
-printE(L)
-alpha = (Mp*kg2J*J2eV)/h/c*L
-printE("alpha", alpha)
+printE("Max wave length (m)", L)
+N_Planck_Mass = (Mp * kg2J * J2eV) / h / c * L * 2
+printE("N of Nth energy equal to Planck mass (unitless)", N_Planck_Mass)
 x = symbols('x', positive=True)
-gap = zeta(4)-Sum(1/x**4, (x, 1, 8.665485e+30)).doit()
-gap = gap * 1e-90
-printE("gap", gap)
-eee = Mp*kg2J
-printE("Planck Mass in Joule", eee)
-printE("Vacuum Energy Density", (3*eee/2)*alpha*gap/(4/3*pi*lp**3))
+Ratio_Planck_Mass = zeta(4) - Sum(1 / x ** 4, (x, 1, 1.733097e+30)).doit()
+# Alternative way to calculate value, because Sum function cannot handle upper bound larger than 1e35
+Ratio_Planck_Mass = Ratio_Planck_Mass * 1e-93 / zeta(4)
+printE("Ratio of energy above Plank mass (unitless)", Ratio_Planck_Mass)
+Planck_Mass_J = Mp * kg2J
+printE("Planck Mass (J)", Planck_Mass_J)
+# E_n = n h c / wave_length / 2
+# k_n = n pi / wave_length
+# p_n = hbar * k_n
+# x_n * p_n = hbar / 2
+# x_n = h c / (pi E_n)
+Length = hJ * c / (pi * Planck_Mass_J)
+printE("Length (m)", Length)
+VED = Planck_Mass_J * N_Planck_Mass * Ratio_Planck_Mass / ((Length) ** 3) * J2kg / 1.6
+printE("Vacuum Energy Density (J/m^3)",
+       VED
+       )
+
+A = (Length) ** 3 / Ratio_Planck_Mass
+B = (4 / 3 * pi) * (L / 2) ** 3
+printE(A)
+printE(B)
+printE(A/B)
